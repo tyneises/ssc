06f1857df6dd3d65fcb249eddcd8137671c5a8e8
Nick DiOrio <nicholas.diorio@nrel.gov>
2016-04-21 19:29:40 +0000

    script to set environmental variables.  Pass in an argument to change to the pvsamv2 workspace, otherwise, pass no variables: e.g, to change to pvsamv2: setenv.bat pvsamv2 to change back to trunk: setenv.bat Note, change the directories in the file to reflect where your pvsamv2 and trunk installations of ssc and SAMnt are

---
+++ setenv.bat
@@ -0,0 +1,7 @@
+if {%1} == {pvsamv2} (
+    setx SSCDIR C:\Users\ndiorio\Documents\Projects\ssc\branches\pvsamv2
+    setx SAMNTDIR C:\Users\ndiorio\Documents\Projects\SAMnt\branches\pvsamv2
+) ELSE (
+    setx SSCDIR C:\Users\ndiorio\Documents\Projects\ssc\trunk
+    setx SAMNTDIR C:\Users\ndiorio\Documents\Projects\SAMnt\trunk
+)
